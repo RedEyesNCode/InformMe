9d10b0b5eba472485aeed54dc903e6953c500a52
RedEyesNCode <ashutosh.singh19071999@gmail.com>
2022-04-22 13:12:53 +0530

    Success Error And Warning Dialogs are completed

--- app/src/main/java/com/redeyesncode/informer/MainActivity.java
+++ app/src/main/java/com/redeyesncode/informer/MainActivity.java
@@ -18,17 +18,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        RedEyesNCode redEyesNCode = new RedEyesNCode(MainActivity.this);
         //Created the default libary module and Added. The libary in the local of the Sample Project itself.
         binding.btnShowSuccessDialog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Making the Object of the class declared in the libary
-                RedEyesNCode redEyesNCode = new RedEyesNCode(MainActivity.this);
-                redEyesNCode.initiateSuccessDialog(MainActivity.this,"Important Alert !","This is a success dialog",false,false);
+
+                redEyesNCode.initiateSuccessDialog(MainActivity.this,"Success !","This is a success dialog",false,false);
 
             }
         });
+        //Creating the default error dialog.
+        binding.btnError.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                redEyesNCode.intiateErrorDialog(MainActivity.this,"Error !","This is a sample error Message !",true,true);
+
+            }
+        });
+        //Creating the Deafault warning dialog.
+        binding.btnWarning.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                redEyesNCode.intiateWarningDialog(MainActivity.this,"Warning !","This is sample warning message !",true,true);
+            }
+        });
+
 
 
 
--- informer/src/main/java/com/redeyesncode/informer/customDialogCode/RedEyesNCode.java
+++ informer/src/main/java/com/redeyesncode/informer/customDialogCode/RedEyesNCode.java
@@ -18,9 +18,61 @@ public class RedEyesNCode {
         this.context = context;
         this.dialog = dialog;
         dialog = new Dialog(context, R.style.RoundedCornersDialog);
-        dialog.setContentView(R.layout.common_dialog_box_success);
+
     }
+    public void intiateErrorDialog(Context context,String errorTitle, String message, boolean isCancelable, boolean isCancelableOutsideTouch){
+        dialog.setContentView(R.layout.common_dialog_box_error);
+        dialog.setCancelable(isCancelable);
+        dialog.setCanceledOnTouchOutside(isCancelableOutsideTouch);
+        dialog.show();
+        TextView textViewMessage, textViewSuccess;
+        textViewMessage = dialog.findViewById(R.id.tvSetText);
+        textViewSuccess = dialog.findViewById(R.id.successTitle);
+        textViewMessage.setText(message);
+        textViewSuccess.setText(errorTitle);
+        LinearLayout btnOk, btnCancel;
+        btnOk = dialog.findViewById(R.id.btnOk);
+        btnCancel = dialog.findViewById(R.id.btnCancel);
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+
+
+    }
+
+    public void intiateWarningDialog(Context context,String warningTitle, String message, boolean isCancelable, boolean isCancelableOutsideTouch){
+        dialog.setContentView(R.layout.common_dialog_box_warning);
+        dialog.setCancelable(isCancelable);
+        dialog.setCanceledOnTouchOutside(isCancelableOutsideTouch);
+        dialog.show();
+        TextView textViewMessage, textViewSuccess;
+        textViewMessage = dialog.findViewById(R.id.tvSetText);
+        textViewSuccess = dialog.findViewById(R.id.successTitle);
+        textViewMessage.setText(message);
+        textViewSuccess.setText(warningTitle);
+        LinearLayout btnOk, btnCancel;
+        btnOk = dialog.findViewById(R.id.btnOk);
+        btnCancel = dialog.findViewById(R.id.btnCancel);
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+
+
+    }
+
+
     public void initiateSuccessDialog(Context context, String successTitle,String message,boolean isCancelable,boolean isCanceledonOutsideTouch){
+        dialog.setContentView(R.layout.common_dialog_box_success);
         dialog.setCancelable(isCancelable);
         dialog.setCanceledOnTouchOutside(isCanceledonOutsideTouch);
         dialog.show();
@@ -31,7 +83,15 @@ public class RedEyesNCode {
         textViewSuccess.setText(successTitle);
 
         LinearLayout btnOk, btnCancel;
+        btnOk = dialog.findViewById(R.id.btnOk);
         btnCancel = dialog.findViewById(R.id.btnCancel);
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
 
 
 
